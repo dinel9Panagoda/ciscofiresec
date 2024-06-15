a766052aa2a5b08642cf6b83c443efa3e4a20d7b
dinel9Panagoda <dinelkapanagoda@gmail.com>
2024-06-15 19:08:14 +0530

    banner text adjustment

--- app/components/Banner/index.tsx
+++ app/components/Banner/index.tsx
@@ -5,8 +5,8 @@ const Banner = () => {
         
             <div className='centered'>
 
-                        <button className='sm:font-bold font-semibold sm:px-5 sm:py-1 px-2 rounded-3xl tracking-wider text-white bg-red sm:text-lg text-xs/[20px] hover:text-red hover:bg-[#fee2e2] hover:shadow-xl sm:mt-22 mt-32'>YOUR PROBLEM SOLVING COMPANY</button>
-                        <h1 className='font-black text-white lg:text-6xl md:text-5xl sm:text-4xl text-3xl py-3'>
+                        <button className='sm:font-bold font-semibold sm:px-5 sm:py-1 px-2 rounded-3xl tracking-wider text-white bg-red sm:text-lg text-xs hover:text-red hover:bg-[#fee2e2] hover:shadow-xl sm:mt-22 mt-20'>YOUR PROBLEM SOLVING COMPANY</button>
+                        <h1 className='font-black text-white lg:text-6xl md:text-5xl sm:text-4xl text-xl py-3'>
                             Cisco Fire Security Communication
                         </h1>
 
