0f11a6e59b1a9297dbdfa90f0488783925bc16e1
dinel9Panagoda <dinelkapanagoda@gmail.com>
2024-06-16 21:05:03 +0530

    Banner title animation

--- app/components/Banner/index.tsx
+++ app/components/Banner/index.tsx
@@ -6,7 +6,7 @@ const Banner = () => {
             <div className='centered'>
 
                         <button className='sm:font-bold font-semibold sm:px-5 sm:py-1 px-2 rounded-3xl tracking-wider text-white bg-red sm:text-lg text-xs hover:text-red hover:bg-[#fee2e2] hover:shadow-xl sm:mt-22 mt-20'>YOUR PROBLEM SOLVING COMPANY</button>
-                        <h1 className='font-black text-white lg:text-6xl md:text-5xl sm:text-4xl text-xl py-3'>
+                        <h1 className='font-black text-white lg:text-6xl md:text-5xl sm:text-4xl text-xl py-3 banner_title_animation'>
                             Cisco Fire Security Communication
                         </h1>
                         
