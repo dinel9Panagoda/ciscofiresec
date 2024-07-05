1458e18de2457947c8dfb50861d130207ed56e12
dinel9Panagoda <dinelkapanagoda@gmail.com>
2024-07-06 00:02:49 +0530

    Prof Services

--- components/Dedicated/index.tsx
+++ components/Dedicated/index.tsx
@@ -2,24 +2,48 @@ import Image from "next/image";
 
 const Dedicated = () => {
     return (
-        <div className="relative">
+        <div className="">
 
-            <Image src="/images/dedicated/spiral.svg" height={272} width={686} alt="spiral-design" className="absolute left-0 hidden lg:block -z-10" />
 
-            <div className='mx-auto max-w-7xl px-4 my-40 sm:py-20 lg:px-8'>
-                <div className='grid grid-cols-1 md:grid-cols-2 my-16'>
+            <div className='mx-2 mx-auto max-w-7xl px-4 my-40 lg:px-8'>
 
-                    {/* COLUMN-1 */}
+                <h1 className="text-red font-semibold text-5xl Wittgenstein text-center">Special offers for the professionals</h1>
+
+                <div className='grid gap-5 lg:grid-cols-5 sm:grid-cols-3 grid-cols-1 my-16'>
+
                     <div>
-                        <Image src="/images/dedicated/man.svg" alt="man-icon" width={416} height={530} className="mx-auto md:mx-0" />
+                        <img src='/images/profservices/architect.webp' alt="Architect" className="rounded-3xl w-full h-52 object-cover" />
+                        <a href="/architects">
+                        <h1 className="bg-grey mt-3 text-center p-3 rounded-3xl text-white uppercase font-extrabold">Architects</h1>
+                        </a>
                     </div>
 
-                    {/* COLUMN-2 */}
-                    <div className="relative">
-                        <Image src="images/dedicated/comma.svg" alt="comma-image" width={200} height={106} className="absolute comma-pos hidden lg:block" />
-                        <h2 className="text-4xl lg:text-65xl pt-4 font-bold sm:leading-tight mt-5 text-center lg:text-start">Dedicated to help peoples design needs.</h2>
-                        <p className="font-medium text-lightblack text-2xl mt-5 text-center lg:text-start">Quis ipsum suspendisse ultrices gravida risus commodo viverra maecenas accumsan lacus vel facilisis</p>
-                        <p className="text-2xl font-semibold mt-12 lg:ml-32 preline text-center lg:text-start"> Cathy Hills, CEO</p>
+                    <div>
+                        <img src='/images/profservices/contractors.jpeg' alt="Contractors" className="rounded-3xl w-full h-52 object-cover" />
+                        <a href="/contractors">
+                        <h1 className="bg-grey mt-3 text-center p-3 rounded-3xl text-white uppercase font-extrabold">Contractors</h1>
+                        </a>
+                    </div>
+
+                    <div>
+                        <img src='/images/profservices/electricians.jpeg' alt="Electricians" className="rounded-3xl w-full h-52 object-cover" />
+                        <a href="/electricians">
+                        <h1 className="bg-grey mt-3 text-center p-3 rounded-3xl text-white uppercase font-extrabold">Electricians</h1>
+                        </a>
+                    </div>
+
+                    <div>
+                        <img src='/images/profservices/realestate.jpeg' alt="Real Estate Agents" className="rounded-3xl w-full h-52 object-cover" />
+                        <a href="/realestateagents">
+                        <h1 className="bg-grey mt-3 text-center p-3 rounded-3xl text-white uppercase font-extrabold">Real Estate Agents</h1>
+                        </a>
+                    </div>
+
+                    <div>
+                        <img src='/images/profservices/buildingmanagers.webp' alt="Building Managers" className="rounded-3xl w-full h-52 object-cover" />
+                        <a href="/buildingmanagers">
+                        <h1 className="bg-grey mt-3 text-center p-3 rounded-3xl text-white uppercase font-extrabold">Building Managers</h1>
+                        </a>
                     </div>
 
                 </div>
