0c66fdc45a8b3bca174bb58a213bf31ce6277737
dinel9Panagoda <dinelkapanagoda@gmail.com>
2024-07-05 22:08:33 +0530

    components transferred

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css';
-import Navbar from './components/Navbar/index';
-import Footer from './components/Footer/index';
+import Navbar from '../components/Navbar/index';
+import Footer from '../components/Footer/index';
 
 
 export const metadata = {
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import Banner from './components/Banner/index';
-import Dedicated from './components/Dedicated/index';
-import Digital from './components/Digital/index';
-import Beliefs from './components/Beliefs/index';
-import Wework from './components/Wework/index';
+import Banner from '../components/Banner/index';
+import Dedicated from '../components/Dedicated/index';
+import Digital from '../components/Digital/index';
+import Beliefs from '../components/Beliefs/index';
+import Wework from '../components/Wework/index';
 // import Ourteam from './components/Ourteam/index';
-import FAQ from './components/FAQ/index';
-import Articles from './components/Articles/index';
-import OurServices from './components/Services/index';
-import SideBar from './components/SideBar/index';
+import FAQ from '../components/FAQ/index';
+import Articles from '../components/Articles/index';
+import OurServices from '../components/Services/index';
+import SideBar from '../components/SideBar/index';
 
 
 export default function Home() {
