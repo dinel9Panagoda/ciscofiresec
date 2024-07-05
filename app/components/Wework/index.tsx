ace603ae04bec234f047a7ec58c8c52c8f356877
dinel9Panagoda <dinelkapanagoda@gmail.com>
2024-07-05 22:04:58 +0530

    Our partners section

--- app/components/Wework/index.tsx
+++ app/components/Wework/index.tsx
@@ -6,41 +6,61 @@ import Image from "next/image";
 // CAROUSEL DATA
 
 interface DataType {
-    profession: string;
-    name: string;
+    // profession: string;
+    // name: string;
     imgSrc: string;
 }
 
 const postData: DataType[] = [
     {
-        profession: 'Co-founder',
-        name: 'John Doe',
-        imgSrc: '/images/wework/avatar.svg',
+        // profession: 'Co-founder',
+        // name: 'John Doe',
+        imgSrc: '/images/partnerbrands/Aiphone.png'
     },
     {
-        profession: 'Co-founder',
-        name: 'John Doe',
-        imgSrc: '/images/wework/avatar3.svg',
+        // profession: 'Co-founder',
+        // name: 'John Doe',
+        imgSrc: '/images/partnerbrands/Axis.png'
     },
     {
-        profession: 'Co-founder',
-        name: 'John Doe',
-        imgSrc: '/images/wework/avatar4.svg',
+        // profession: 'Co-founder',
+        // name: 'John Doe',
+        imgSrc: '/images/partnerbrands/Bosch.png'
     },
     {
-        profession: 'Co-founder',
-        name: 'John Doe',
-        imgSrc: '/images/wework/avatar.svg',
+        // profession: 'Co-founder',
+        // name: 'John Doe',
+        imgSrc: '/images/partnerbrands/Honeywell.png'
     },
     {
-        profession: 'Co-founder',
-        name: 'John Doe',
-        imgSrc: '/images/wework/avatar3.svg',
+        // profession: 'Co-founder',
+        // name: 'John Doe',
+        imgSrc: '/images/partnerbrands/Napco.png'
     },
     {
-        profession: 'Co-founder',
-        name: 'John Doe',
-        imgSrc: '/images/wework/avatar4.svg',
+        // profession: 'Co-founder',
+        // name: 'John Doe',
+        imgSrc: '/images/partnerbrands/Panasonic.png'
+    },
+    {
+        // profession: 'Co-founder',
+        // name: 'John Doe',
+        imgSrc: '/images/partnerbrands/Potter.png'
+    },
+    {
+        // profession: 'Co-founder',
+        // name: 'John Doe',
+        imgSrc: '/images/partnerbrands/Siemens.png'
+    },
+    {
+        // profession: 'Co-founder',
+        // name: 'John Doe',
+        imgSrc: '/images/partnerbrands/SilentKnight.png'
+    },
+    {
+        // profession: 'Co-founder',
+        // name: 'John Doe',
+        imgSrc: '/images/partnerbrands/SystemSensor.png'
     },
 ]
 
@@ -99,9 +119,9 @@ export default class MultipleItems extends Component {
                 <div className='mx-auto max-w-2xl lg:max-w-7xl sm:py-4 lg:px-8 '>
 
                     <div className="text-center">
-                        <h3 className="text-4xl sm:text-6xl font-bold text-black my-2">We work in several verticals.</h3>
-                        <h3 className="text-4xl sm:text-6xl font-bold text-black opacity-50 lg:mr-48 my-2">We work in several verticals.</h3>
-                        <h3 className="text-4xl sm:text-6xl font-bold text-black opacity-25 lg:-mr-32 my-2">We work in several verticals.</h3>
+                        <h3 className="text-4xl sm:text-6xl font-bold text-black my-2">Our partners</h3>
+                        <h3 className="text-4xl sm:text-6xl font-bold text-black opacity-50 lg:mr-48 my-2">Our partners</h3>
+                        <h3 className="text-4xl sm:text-6xl font-bold text-black opacity-25 lg:-mr-32 my-2">Our partners</h3>
                     </div>
 
                 </div>
@@ -109,13 +129,13 @@ export default class MultipleItems extends Component {
                 <Slider {...settings}>
                     {postData.map((items, i) => (
                         <div key={i}>
-                            <div className='bg-white m-3 py-14 my-10 text-center shadow-xl rounded-3xl'>
+                            <div className='bg-white m-3 py-10 my-10 text-center shadow-xl rounded-3xl'>
                                 <div className='relative'>
                                     <Image src={items.imgSrc} alt="gaby" width={182} height={182} className="inline-block m-auto" />
-                                    <Image src={'/images/wework/linkedin.svg'} alt="greenbg" width={120} height={120} className=" absolute inline-block position-linkedin" />
+                                    {/* <Image src={'/images/wework/linkedin.svg'} alt="greenbg" width={120} height={120} className=" absolute inline-block position-linkedin" /> */}
                                 </div>
-                                <h4 className='text-4xl font-bold pt-14'>{items.name}</h4>
-                                <h3 className='text-2xl font-normal pt-4 pb-2 opacity-50'>{items.profession}</h3>
+                                {/* <h4 className='text-4xl font-bold pt-14'>{items.name}</h4> */}
+                                {/* <h3 className='text-2xl font-normal pt-4 pb-2 opacity-50'>{items.profession}</h3> */}
                             </div>
                         </div>
                     ))}
