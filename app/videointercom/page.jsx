95c83346d396ba14c69f380c73f71bc95c52a8b4
dinel9Panagoda <dinelkapanagoda@gmail.com>
2024-07-12 00:26:51 +0530

    Video Intercom image added

--- app/videointercom/page.jsx
+++ app/videointercom/page.jsx
@@ -10,7 +10,7 @@ const VideoIntercom = () => {
 
         <div className='grid sm:grid-cols-2 gap-7'>
             <div>
-                <img src='/images/services/videointercom1.webp' alt="Video Intercom" className="inline-block rounded-3xl w-full" />
+                <img src='/images/services/videoIntercom1.webp' alt="Video Intercom" className="inline-block rounded-3xl w-full" />
             </div>
             <div className='text-justify'>
             Video intercom systems provide a modern and convenient way to manage access and communication in various settings, including residential buildings, oces, and commercial spaces. These systems typically consist of a doorbell camera or intercom unit installed at an entry point, such as a front door or gate, and an indoor monitor or mobile app that allows occupants to see and communicate with visitors remotely. With built-in cameras and speakers, video intercoms enable visual verication of visitors before granting access, enhancing security and peace of mind for occupants. Additionally, some advanced video intercom systems oer features such as motion detection, night vision, and two-way audio communication, allowing for seamless interaction even in low-light conditions or noisy environments. Whether it's screening visitors, granting access to delivery personnel, or communicating with guests, video intercoms provide a convenient and ecient solution for managing access and enhancing communication in both residential and commercial settings. As technology continues to evolve, video intercom systems are becoming increasingly integrated with smart home and building automation systems, oering enhanced functionality and connectivity for a seamless and secure living and working environment.
