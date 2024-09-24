1ae1c3ef13188a5934187d3cbc040eb77419536e
dinel9Panagoda <dinelkapanagoda@gmail.com>
2024-09-24 14:02:54 +0530

    3

--- app/cctv/page.tsx
+++ app/cctv/page.tsx
@@ -1,49 +1,45 @@
 "use client"
-import React from 'react'
-import EmailIcon from '@mui/icons-material/Email';
-import PhoneIcon from '@mui/icons-material/Phone';
+import React, { useState } from 'react'
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import Modal from '@mui/material/Modal';
 
 import Slider from "react-slick";
+import RecentWork from '@/components/RecentWorkIcon';
 
 // CAROUSEL DATA
 
 interface DataType {
-    // profession: string;
-    // name: string;
     imgSrc: string;
 }
 
 const postData: DataType[] = [
     {
-        // profession: 'Co-founder',
-        // name: 'John Doe',
-        imgSrc: '/images/services/CCTV/cctv1.jpg'
+        imgSrc: '/images/services/cctv/CCTV1.jpeg'
     },
     {
-        // profession: 'Co-founder',
-        // name: 'John Doe',
-        imgSrc: '/images/services/CCTV/cctv2.jpg'
+        imgSrc: '/images/services/cctv/CCTV2.jpeg'
     },
     {
-        // profession: 'Co-founder',
-        // name: 'John Doe',
-        imgSrc: '/images/services/CCTV/cctv3.jpg'
+        imgSrc: '/images/services/cctv/CCTV3.jpeg'
     },
     {
-        // profession: 'Co-founder',
-        // name: 'John Doe',
-        imgSrc: '/images/services/CCTV/cctv1.jpg'
+        imgSrc: '/images/services/cctv/CCTV4.jpeg'
     },
     {
-        // profession: 'Co-founder',
-        // name: 'John Doe',
-        imgSrc: '/images/services/CCTV/cctv2.jpg'
+        imgSrc: '/images/services/cctv/CCTV5.jpeg'
     },
     {
-        // profession: 'Co-founder',
-        // name: 'John Doe',
-        imgSrc: '/images/services/CCTV/cctv3.jpg'
+        imgSrc: '/images/services/cctv/CCTV6.jpeg'
     },
+    {
+        imgSrc: '/images/services/cctv/CCTV7.jpeg'
+    },
+    {
+        imgSrc: '/images/services/cctv/CCTV8.jpeg'
+    },
+    {
+        imgSrc: '/images/services/cctv/CCTV9.jpeg'
+    }
 ]
 
 const CCTV = () => {
@@ -89,33 +85,92 @@ const CCTV = () => {
             }
         ]
     };
+        // Modal
+        const [open, setOpen] = useState(false);
 
-  return (
-    <section className='py-32 bg-cctv'>
-
-<div className='sm:px-10 px-5'>
-<h1 className='text-red font-semibold text-5xl Wittgenstein text-center pb-20 xl:text-6xl'>CCTV</h1>
-
-      
-
-<div className='grid sm:grid-cols-2 gap-7'>
-    <div>
-        <img src='/images/services/CCTV.jpg' alt="CCTV" className="inline-block rounded-3xl w-full" />
-    </div>
-    <div className='text-justify'>
-    Closed-circuit television (CCTV) systems are ubiquitous in today's society, serving as a crucial tool for surveillance, security, and monitoring in various settings. Consisting of cameras, monitors, and recording devices, CCTV systems enable the capture and observation of video footage within a designated area, whether it's a public space, commercial establishment, or residential property.
-
-    </div>
-
-</div>
-
-<h6 className='text-red text-center pt-20 pb-5'>Recent CCTV Units We Did For Our Clients...</h6>
-
-</div>
-
+        const handleClose = () => {
+            setOpen(false);
+        };
+    
+        const handleOpen = () => {
+            setOpen(true);
+        };
+return (
+    <section className=''>
         
+        {/* <div className=''> */}
+        <div className='mb-32'>
+            <img className='w-full' src='/images/services/cctv/CCTVMainBanner.png' />
 
-<Slider {...settings}>
+        </div>
+
+        <div className='grid sm:grid-cols-2 gap-7 px-10 max-w-7xl mx-auto'>
+            <div>
+                <img src='/images/services/cctv/StreetCCTV.jpeg' alt="cctv" className="inline-block rounded-3xl w-full"  />
+            </div>
+            <div className='text-justify'>
+                <img className='inline-block w-auto' src='/images/services/cctv/CCTV.GIF' /><br />
+                Closed-circuit television (CCTV) systems are ubiquitous in today's society, serving as a crucial tool for surveillance, security, and monitoring in various settings. Consisting of cameras, monitors, and recording devices, CCTV systems enable the capture and observation of video footage within a designated area, whether it's a public space, commercial establishment, or residential property.
+
+
+            </div>
+
+        </div>
+                <div className='specialOfferSideBar'>
+                    <div onClick={handleOpen} className='justify-self-end cursor-pointer mt-10 ml-5'>
+
+                    <div className=' relative w-40 h-auto bg-blue rounded-3xl drop-shadow-[0_15px_15px_rgba(0,0,0,0.8)]'>
+                        <span className="relative absolute top-1 left-1 flex h-10 w-10">
+                            <span className=" animate-ping absolute inline-flex h-full w-full rounded-full bg-white opacity-75"></span>
+
+                                <span className="relative inline-flex rounded-full h-10 w-10 bg-white">
+                                    <img src='/icons/discount-shape.svg' className='w-10' />
+                                </span>
+                            </span>
+
+                            <img src='/icons/arrow-circle-down-left.svg' className='w-12 absolute top-0 right-0' />           
+                            <p className='pt-5 px-5 text-white font-black text-xl '>Special Offers</p>
+                            <p className='text-lightblue text-xs mb-1 pl-5'>Click here...</p>
+                            
+                            <img className='w-80 rounded-3xl bg-white p-3' src='/images/services/cctv/SpecialOfferCCTV.gif' />
+                        </div>
+
+</div>
+                </div>
+    <RecentWork />               
+     <Modal
+                        open={open}
+                        onClose={handleClose}
+                        disableAutoFocus={true}
+                        className="absolute w-fit h-fit m-auto text-center"
+                    >
+
+                        <div className='grid md:grid-cols-2'>
+                            <div className='bg-white md:rounded-l-3xl rounded-t-3xl mx-auto'>
+                                <img className='md:w-96 w-56' src='/images/services/firealarms/SpecialOfferPopUp.gif' alt='Special Offer' />    
+                            </div>
+                            <div className='bg-blue md:rounded-r-3xl relative w-96'>
+                            <div onClick={handleClose} className='absolute top-5 right-5 text-white font-bold cursor-pointer'>X</div>
+                                <div className='absolute -left-12 -top-12 p-3 bg-white w-fit rounded-full border-2 border-blue  drop-shadow-[0_3px_3px_rgba(0,0,0,0.8)]'>
+                                <img src='/images/services/cctv/SpecialOfferCCTV.gif' className='w-20' />
+                                </div>
+                                <h1 className='uppercase font-black text-3xl py-10 text-white'>Special Offer!</h1>
+
+                                <p className=''><span className='font-bold bg-lightblue px-5 py-1 rounded-full border-2 border-navyblue text-navyblue text-sm'>Stay Safe & Save Big!</span> <br/> <br/> <span className=' text-white px-5'>Limited Time Discounts on Essential CCTV Equipment!</span></p>
+                                <div className='mt-10 mx-auto bg-white w-fit py-5 px-10 rounded-full text-blue border-2 mb-5 border-darkblue drop-shadow-[0_3px_3px_rgba(0,0,0,0.8)]'>
+                                    <span className='text-7xl font-black'>30%</span>
+                                    <span className='ml-4'>OFF</span>
+                                </div>
+                            </div>
+                        </div>
+
+                
+
+                    </Modal>
+        <h6 className='text-red text-center pb-5 font-bold text-3xl'>Recent CCTV Units We Did For Our Clients...</h6>
+
+
+        <Slider {...settings}>
                     {postData.map((items, i) => (
                         <div key={i}>
                             <div className='bg-white m-3 mb-32 mt-10 text-center shadow-3xl rounded-3xl'>
--- app/firealarms/page.tsx
+++ app/firealarms/page.tsx
@@ -1,60 +1,85 @@
 "use client"
 import React from 'react'
-import EmailIcon from '@mui/icons-material/Email';
-import PhoneIcon from '@mui/icons-material/Phone';
-import Image from "next/image";
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import RecentWork from '../../components/RecentWorkIcon/index'
 
 import Slider from "react-slick";
 
 // CAROUSEL DATA
 
 interface DataType {
-    // profession: string;
-    // name: string;
     imgSrc: string;
 }
 
 const postData: DataType[] = [
     {
-        // profession: 'Co-founder',
-        // name: 'John Doe',
-        imgSrc: '/images/services/firealarms/fa1.jpg'
+        imgSrc: '/images/services/firealarms/fa1.jpeg'
     },
     {
-        // profession: 'Co-founder',
-        // name: 'John Doe',
-        imgSrc: '/images/services/firealarms/fa2.jpg'
+        imgSrc: '/images/services/firealarms/fa2.jpeg'
     },
     {
-        // profession: 'Co-founder',
-        // name: 'John Doe',
-        imgSrc: '/images/services/firealarms/fa3.jpg'
+        imgSrc: '/images/services/firealarms/fa3.jpeg'
     },
     {
-        // profession: 'Co-founder',
-        // name: 'John Doe',
-        imgSrc: '/images/services/firealarms/fa4.jpg'
+        imgSrc: '/images/services/firealarms/fa4.jpeg'
     },
     {
-        // profession: 'Co-founder',
-        // name: 'John Doe',
-        imgSrc: '/images/services/firealarms/fa1.jpg'
+        imgSrc: '/images/services/firealarms/fa5.jpeg'
     },
     {
-        // profession: 'Co-founder',
-        // name: 'John Doe',
-        imgSrc: '/images/services/firealarms/fa2.jpg'
+        imgSrc: '/images/services/firealarms/fa6.jpg'
     },
     {
-        // profession: 'Co-founder',
-        // name: 'John Doe',
-        imgSrc: '/images/services/firealarms/fa3.jpg'
+        imgSrc: '/images/services/firealarms/fa7.jpeg'
     },
     {
-        // profession: 'Co-founder',
-        // name: 'John Doe',
-        imgSrc: '/images/services/firealarms/fa4.jpg'
+        imgSrc: '/images/services/firealarms/fa8.jpeg'
+    },
+    {
+        imgSrc: '/images/services/firealarms/fa9.jpeg'
+    },
+    {
+        imgSrc: '/images/services/firealarms/fa10.jpeg'
+    },
+    {
+        imgSrc: '/images/services/firealarms/fa11.jpeg'
+    },
+    {
+        imgSrc: '/images/services/firealarms/fa12.jpg'
+    },
+    {
+        imgSrc: '/images/services/firealarms/fa13.jpeg'
+    },
+    {
+        imgSrc: '/images/services/firealarms/fa14.jpeg'
+    },
+    {
+        imgSrc: '/images/services/firealarms/fa15.jpeg'
+    },
+    {
+        imgSrc: '/images/services/firealarms/fa16.jpeg'
+    },
+    {
+        imgSrc: '/images/services/firealarms/fa17.jpeg'
+    },
+    {
+        imgSrc: '/images/services/firealarms/fa18.jpeg'
+    },
+    {
+        imgSrc: '/images/services/firealarms/fa19.jpeg'
+    },
+    {
+        imgSrc: '/images/services/firealarms/fa20.jpeg'
+    },
+    {
+        imgSrc: '/images/services/firealarms/fa21.jpeg'
+    },
+    {
+        imgSrc: '/images/services/firealarms/fa22.jpeg'
+    },
+    {
+        imgSrc: '/images/services/firealarms/fa23.jpeg'
     }
 ]
 
@@ -102,59 +127,49 @@ const FireAlarms = () => {
         ]
     };
 
-  return (
-    <section className='py-32 bg-firealarms'>
+return (
+    <section className=''>
         
-        <div className='sm:px-10 px-10'>
-        <h1 className='text-red font-semibold text-5xl Wittgenstein text-center pb-20 xl:text-6xl'>FIRE ALARMS</h1>
+        {/* <div className=''> */}
+        <div className='mb-32'>
+            <img className='w-full' src='/images/services/firealarms/FireAlarmMainBanner.png' />
 
-        {/* <div className='flex flex-wrap pb-20 gap-3 justify-center items-center'>
-            <div className='bg-lightred px-3 py-3 rounded-3xl text-red '>School</div>
-            <div className='bg-lightred px-3 py-3 rounded-3xl text-red'>Buildings</div>
-            <div className='bg-lightred px-3 py-3 rounded-3xl text-red '>Office</div>
-            <div className='bg-lightred px-3 py-3 rounded-3xl text-red '>Educational</div>
-            <div className='bg-lightred px-3 py-3 rounded-3xl text-red '>Institution</div>
-            <div className='bg-lightred px-3 py-3 rounded-3xl text-red '>School</div>
-            <div className='bg-lightred px-3 py-3 rounded-3xl text-red '>Residencial</div>
-            <div className='bg-lightred px-3 py-3 rounded-3xl text-red '>Ware houses</div>
-            <div className='bg-lightred px-3 py-3 rounded-3xl text-red '>Many more...</div>
+        </div>
 
-        </div> */}
-
-        <div className='grid sm:grid-cols-2 gap-7'>
+        <div className='grid sm:grid-cols-2 gap-7 px-10 max-w-7xl mx-auto'>
             <div>
-                <img src='/images/services/fireAlarms.webp' alt="Fire Alarms" className="inline-block rounded-3xl w-full" />
+                <img src='/images/services/firealarms/FireAlarmOperator.jpeg' alt="Fire Alarms" className="inline-block rounded-3xl w-full"  />
             </div>
             <div className='text-justify'>
-            Fire alarms are critical safety devices designed to detect the presence of smoke, heat, or ames, signaling potential danger and prompting timely evacuation. These alarms serve as the rst line of defense in safeguarding lives and property against the devastating eects of re. Equipped with advanced sensors, modern re alarm systems are capable of detecting even the slightest traces of smoke, ensuring early detection and swift response to emergencies.
+                <img className='inline-block w-auto pb-10' src='/images/services/firealarms/FIRE_ALARMS.png' /><br />
+                Fire alarms are critical safety devices designed to detect the presence of smoke, heat, or flames, signaling potential danger and prompting timely evacuation. These alarms serve as the first line of defense in safeguarding lives and property against the devastating effects of fire. Equipped with advanced sensors, modern fire alarm systems are capable of detecting even the slightest traces of smoke, ensuring early detection and swift response to emergencies.
 
-            <div className='grid grid-cols-2 gap-5 mt-10 text-navyblue ml-5 text-sm'>
-                <ul>
-                    <a href='/firealarms/schools'><li className='service_list_item cursor-pointer'><ArrowCircleRightIcon fontSize='large' className='m-1'/>Educational</li></a>
+                <div className='grid grid-cols-2 gap-5 mt-10 text-navyblue ml-5 text-sm'>
+                    <ul>
+                        <a href='/firealarms/schools'><li className='service_list_item_fa cursor-pointer'><ArrowCircleRightIcon fontSize='large' className='m-1'/>Educational</li></a>
                     
-                     <a href='/firealarms/buildings'><li className='service_list_item cursor-pointer'><ArrowCircleRightIcon fontSize='large' className='m-1'/>Buildings</li></a>
+                        <a href='/firealarms/buildings'><li className='service_list_item_fa cursor-pointer'><ArrowCircleRightIcon fontSize='large' className='m-1'/>Buildings</li></a>
 
-                    <a href='/firealarms/offices'><li className='service_list_item cursor-pointer'><ArrowCircleRightIcon fontSize='large' className='m-1'/>Offices</li></a>
+                        <a href='/firealarms/offices'><li className='service_list_item_fa cursor-pointer'><ArrowCircleRightIcon fontSize='large' className='m-1'/>Offices</li></a>
 
-                </ul>
+                    </ul>
 
-                <ul>
-                 <a href='/firealarms/institutions'><li className='service_list_item cursor-pointer'><ArrowCircleRightIcon fontSize='large' className='m-1'/>Institutions</li></a>
+                    <ul>
+                        <a href='/firealarms/institutions'><li className='service_list_item_fa cursor-pointer'><ArrowCircleRightIcon fontSize='large' className='m-1'/>Institutions</li></a>
 
-                <a href='/firealarms/residential'><li className='service_list_item cursor-pointer'><ArrowCircleRightIcon fontSize='large' className='m-1'/>Residential</li></a>
+                        <a href='/firealarms/residential'><li className='service_list_item_fa cursor-pointer'><ArrowCircleRightIcon fontSize='large' className='m-1'/>Residential</li></a>
 
-                 <a href='/firealarms/warehouses'><li className='service_list_item cursor-pointer'><ArrowCircleRightIcon fontSize='large' className='m-1'/>Warehouses</li></a>
+                        <a href='/firealarms/warehouses'><li className='service_list_item_fa cursor-pointer'><ArrowCircleRightIcon fontSize='large' className='m-1'/>Warehouses</li></a>
 
-                {/* <li className='service_list_item_many_more cursor-pointer'><ArrowCircleRightIcon fontSize='large' className='m-1'/>Many more...</li> */}
-                </ul>
-            </div>
+                    </ul>
+                </div>
 
             </div>
 
         </div>
 
-        <h6 className='text-red text-center pt-20 pb-5'>Recent Fire Alarm Units We Did For Our Clients...</h6>
-        </div>
+        <RecentWork />
+        <h6 className='text-red text-center pb-5 font-bold text-3xl'>Recent Fire Alarm Units We Did For Our Clients...</h6>
 
 
         <Slider {...settings}>
